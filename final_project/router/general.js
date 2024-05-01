033e38c13e791252d6e98f4df6b8d85dc3950623
4ditya-R <adityaraja.of@gmail.com>
2024-05-01 14:28:52 -0400

    intial changes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,9 +29,14 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  let isbn = req.params.isbn;
-  return res.status(300).json({message: books[isbn]});
- });
+  const isbn = req.params.isbn;
+  for (let key in books){
+    if(books.hasOwnProperty(key)){
+        value = books[key];
+        return res.status(300).json({message: books[isbn]});
+ }
+}
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
