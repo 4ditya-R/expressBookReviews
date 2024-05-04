3118661c7d11fd45e0b0263e6fb58b5b33920c56
4ditya-R <adityaraja.of@gmail.com>
2024-05-04 14:41:33 -0400

    changes to general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,21 +27,36 @@ public_users.get('/',function (req, res) {
   return res.status(300).json({Message: books});
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const reqIsbn = req.params.isbn;
-  const bookNum = [];
+public_users.get('/isbn/:isbn',async function (req, res) {
+    try {
+        const reqIsbn = req.params.isbn;
+        const booksByIsbn = await getBooksByIsbn(reqIsbn);
 
-  for (const key in books){
-    if(books.hasOwnProperty(key)){
-        if(key === reqIsbn){
+        if (booksByIsbn.length > 0) {
+            res.json({ "Books by ISBN": booksByIsbn });
+        } else {
+            res.status(404).json({ error: "No books found with the specified ISBN" });
+        }
+    } catch (error) {
+        console.error("Error occurred:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+async function getBooksByIsbn(reqIsbn) {
+    const booksByIsbn = [];
+
+    for (const key in books) {
+        if (books.hasOwnProperty(key)) {
             const book = books[key];
-            bookNum.push(book);
+            if (book.isbn === reqIsbn) {
+                booksByIsbn.push(book);
+            }
         }
     }
-  }
-    res.json({"Books by ISBN" : bookNum});
-});
+
+    return booksByIsbn;
+}
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
