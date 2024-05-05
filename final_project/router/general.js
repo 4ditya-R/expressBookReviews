6ca4ad382912ac15ce9313257ee6e2b563990332
4ditya-R <adityaraja.of@gmail.com>
2024-05-05 15:19:04 -0400

    Final Changes to Project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,67 +30,82 @@ public_users.get('/',function (req, res) {
 
 public_users.get('/isbn/:isbn', function (req, res) {
 
-    const reqIsbn = req.params.isbn;
-
-    getBooksByIsbn(reqIsbn)
-        .then(booksByIsbn => {
-            res.json({ "Books by ISBN": booksByIsbn });
-        })
-        .catch(error => {
-            console.error("Error occurred:", error);
-            res.status(500).json({ error: "Internal Server Error" });
-        });
-});
-
-function getBooksByIsbn(reqIsbn) {
-    
-    return new Promise((resolve, reject) => {
-        const booksByIsbn = [];
-
-        for (const key in books) {
-            if(books.hasOwnProperty(key)){
-                const book = books[key];
-                if(book.isbn === reqIsbn){
-                    booksByIsbn.push(book);
-                }
-            }
+    const getBooksByIsbn = new Promise((resolve, reject) => {
+        const isbn = req.params.isbn;
+            if (req.params.isbn) {
+            resolve(res.send(books[isbn]));
         }
-        resolve(booksByIsbn);
+            else {
+                reject(res.send('ISBN not found'));
+            }
+        });
+        getBooksByIsbn.
+            then(function(){
+                console.log("Promise for Task 11 has been resolved");
+       }).
+            catch(function () { 
+                    console.log('ISBN not found');
+      });    
     });
-}
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
 
-   let reqAuthor = req.params.author;
-   let authVal = [];
 
-       for ( const key in books){
-        if(books.hasOwnProperty(key)){
-            const book = books[key];
-            if(book.author === reqAuthor){
-                authVal.push(book);
-            }
-        }   
-    }
-       res.json({'Books By Author': authVal});
-});
+    const get_books_author = new Promise((resolve, reject) => {
+
+        let booksbytitle = [];
+        let isbns = Object.keys(books);
+        isbns.forEach((isbn) => {
+          if(books[isbn]["author"] === req.params.author) {
+            booksbytitle.push({"isbn":isbn,
+                                "title":books[isbn]["title"],
+                                "reviews":books[isbn]["reviews"]});
+          resolve(res.send(JSON.stringify({booksbytitle}, null, 4)));
+          }
+    
+    
+        });
+        reject(res.send("The mentioned author does not exist "))
+            
+        });
+    
+        get_books_author.then(function(){
+                console.log("Promise is resolved");
+       }).catch(function () { 
+                    console.log('The mentioned author does not exist');
+      }); 
+      });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const reqTitle = req.params.title;
-    const titles = [];
+
+
+    const get_books_title = new Promise((resolve, reject) => {
+
+        let booksbyauthor = [];
+        let isbns = Object.keys(books);
+        isbns.forEach((isbn) => {
+          if(books[isbn]["author"] === req.params.author) {
+            booksbyauthor.push({"isbn":isbn,
+                                "title":books[isbn]["title"],
+                                "reviews":books[isbn]["reviews"]});
+          resolve(res.send(JSON.stringify({booksbytitle}, null, 4)));
+          }
     
-    for (const key in books) {
-        if (books.hasOwnProperty(key)) {
-            const book = books[key];
-            if (book.title ===reqTitle ) {
-                titles.push(book);
-            }
-        }
-    }  
-    res.json({ "Books by Title": titles});
-});
+    
+        });
+        reject(res.send("The mentioned author does not exist "))
+            
+        });
+    
+        get_books_title.then(function(){
+                console.log("Promise is resolved");
+       }).catch(function () { 
+                    console.log('The mentioned author does not exist');
+      });
+    
+      });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
